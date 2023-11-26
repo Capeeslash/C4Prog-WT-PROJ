1051c893c03ace9e992f5e8e3c6cc41ad77440da
Capeeslash <kenyoti.p@gmail.com>
2023-11-26 18:50:25 +0800

    feat(home-page): created home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './*.html',
+    'index.html',
+    './Contact%20page/contact.html',
+    './About%20page/about.html',
+    './Projects%20page/projects.html',
+    './Sample%20Projects/mywork.html',
+    './Resume/resume.html'
+  ],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'redwood': '#000000',
+        'mint': '#087F8C',
+        'dark-cyan': '#DADDE0',
+        'sunset': '#FFFFFF',
+      },
+    },
   },
   plugins: [],
-}
+};
+
 
